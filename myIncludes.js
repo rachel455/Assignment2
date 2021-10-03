c164be00ff2116b915ac328dee6550e8490d9192
rachel455 <rachelishak@gmail.com>
2021-10-02 23:59:50 -0400

    test myIncludes function along with the native function

--- myIncludes.js
+++ myIncludes.js
@@ -5,10 +5,25 @@
 
 // INCLUDES //
 Array.prototype.myIncludes = function(includedElement, index) {
-    for(let i = (index || 0); i < this.length; i++){
-        if(this[i] === includedElement){
-            return true;
+    for(let i = (index || 0); i < this.length; i++){ // created for loop to loop through array
+        if(this[i] === includedElement){ //determine whether an array includes a certain value among its entries
+            return true; //return boolean true if value is in array
         }
     }
-    return false;
+    return false; //return boolean false if value is NOT in array
 };
+
+//Test function along with the native function //
+
+const array1 = [1, 2, 3];
+const pets = ['cat', 'dog', 'bat'];
+
+console.log("myIncludes:")
+console.log(array1.myIncludes(2)); // expected output: true
+console.log(pets.myIncludes('cat')); // expected output: true
+console.log(pets.myIncludes('at')); // expected output: false
+
+console.log("includes:")
+console.log(array1.includes(2)); // expected output: true
+console.log(pets.includes('cat')); // expected output: true
+console.log(pets.includes('at')); // expected output: false
