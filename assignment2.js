1baadb3fac6e8ba159599c88ce40965c8184d282
rachel455 <rachelishak@gmail.com>
2021-10-03 01:48:29 -0400

    added myIncludes function to assignment2.js test file

--- assignment2.js
+++ assignment2.js
@@ -29,8 +29,13 @@ Array.prototype.myReduce = function() {
 };
 
 // INCLUDES //
-Array.prototype.myIncludes = function() {
-
+Array.prototype.myIncludes = function(includedElement, index) {
+    for(let i = (index || 0); i < this.length; i++){ // created for loop to loop through array
+        if(this[i] === includedElement){ //determine whether an array includes a certain value among its entries
+            return true; //return boolean true if value is in array
+        }
+    }
+    return false; //return boolean false if value is NOT in array
 };
 
 // INDEXOF //
