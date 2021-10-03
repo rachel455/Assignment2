5582052840b24b5b784d7f0e73f88585d9f78688
rachel455 <rachelishak@gmail.com>
2021-10-03 01:12:44 -0400

    added template of grabvalues function

--- grabValues.js
+++ grabValues.js
@@ -1,3 +1,8 @@
 //Without using the native “Object.values()” method of JavaScript, 
 //compose a function titled “grabValues” that will take in an object and 
 //return all of the values of the key:value pairs of that object.
+
+// VALUES //
+Object.grabValues = function() {
+
+};
