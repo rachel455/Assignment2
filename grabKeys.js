7804518182c8c11360945cca685c85c7d600daf5
rachel455 <rachelishak@gmail.com>
2021-10-03 00:57:55 -0400

    added template of grabKeys function

--- grabKeys.js
+++ grabKeys.js
@@ -1,3 +1,8 @@
 //Without using the native “Object.keys()” method of JavaScript, 
 //compose a function titled “grabKeys” that will take in an object and 
 //return all of the keys of the key:value pairs of that object.
+
+// KEYS //
+Object.grabKeys = function() {
+
+};
