45172eeac59632f5596cb4c0ee3875afaf742b02
rachel455 <rachelishak@gmail.com>
2021-10-03 01:45:53 -0400

    test myPush function along with the native function

--- myPush.js
+++ myPush.js
@@ -5,10 +5,28 @@
 // PUSH //
 Array.prototype.myPush = function(...args){
     let arg_i = 0;
-    let length = this.length;
-    for(let i = length; i < length + args.length; i++){
-        this[i] = args[arg_i];
+    let length = this.length; // uses length property to determine where to start inserting the given values
+    for(let i = length; i < length + args.length; i++){ // created for loop to loop through array
+        this[i] = args[arg_i]; // append values to an array
         arg_i++;
     }
-    return this.length;
+    return this.length; // returns the new length of the array
 };
+
+//Test function along with the native function //
+
+const animals = ['pigs', 'goats', 'sheep'];
+
+console.log("myPush:")
+const count = animals.myPush('cows');
+console.log(count); // expected output: 4
+console.log(animals); // expected output: Array ["pigs", "goats", "sheep", "cows"]
+animals.myPush('chickens', 'cats', 'dogs');
+console.log(animals); // expected output: Array ["pigs", "goats", "sheep", "cows", "chickens", "cats", "dogs"]
+
+console.log("push:")
+const counts = animals.push('cows');
+console.log(count); // expected output: 4
+console.log(animals); // expected output: Array ["pigs", "goats", "sheep", "cows"]
+animals.push('chickens', 'cats', 'dogs');
+console.log(animals); // expected output: Array ["pigs", "goats", "sheep", "cows", "chickens", "cats", "dogs"]
