d901f3afd220d262eea1832bbecb7c8a9a521000
rachel455 <rachelishak@gmail.com>
2021-10-03 00:50:52 -0400

    test myLastIndexOf function along with the native function

--- myLastIndexOf.js
+++ myLastIndexOf.js
@@ -4,10 +4,22 @@
 
 // LASTINDEXOF //
 Array.prototype.myLastIndexOf = function(includedElement, index){
-    for(let i = this.length - 1; i >= (index || 0); i--){
-        if(this[i] == includedElement){
-            return i;
+    for(let i = this.length - 1; i >= (index || 0); i--){ // created for loop to loop through array
+        if(this[i] === includedElement){ //find element in array
+            return i; //eturns the last index at which a given element can be found in the array
         }
-        return -1;
     }
+    return -1; // return -1 if it is not present
 };
+
+//Test function along with the native function //
+
+const animals = ['Dodo', 'Tiger', 'Penguin', 'Dodo'];
+
+console.log("myLastIndexOf:")
+console.log(animals.myLastIndexOf('Dodo')); // expected output: 3
+console.log(animals.myLastIndexOf('Tiger')); // expected output: 1
+
+console.log("lastIndexOf:")
+console.log(animals.lastIndexOf('Dodo')); // expected output: 3
+console.log(animals.lastIndexOf('Tiger')); // expected output: 1
