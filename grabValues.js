a0cc0238eb1621162b636d394425b228b5c12f28
rachel455 <rachelishak@gmail.com>
2021-10-03 01:21:20 -0400

    test grabValues function along with the native function

--- grabValues.js
+++ grabValues.js
@@ -4,9 +4,23 @@
 
 // VALUES //
 Object.grabValues = function(objectList) {
-    let valuesArray = [];
-    for (let value in objectList){
-        value = valuesArray.push(objectList[value]);
+    let valuesArray = []; // make an array
+    for (let value in objectList){ // created for loop to loop through array
+        value = valuesArray.push(objectList[value]); // returns an array of a given object's own enumerable property values
     }
-    return valuesArray;
+    return valuesArray; //returns an array containing the given object's own enumerable property values
 };
+
+//Test function along with the native function //
+
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+
+console.log("grabValues:")
+console.log(Object.grabValues(object1));// expected output: Array ["somestring", 42, false]
+
+console.log("values:")
+console.log(Object.values(object1)); // expected output: Array ["somestring", 42, false]
