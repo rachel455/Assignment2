a81b505368d8357a8eaca1c47d85ee68f028077d
rachel455 <rachelishak@gmail.com>
2021-10-03 00:18:05 -0400

    created myIndexOf function

--- myIndexOf.js
+++ myIndexOf.js
@@ -3,6 +3,11 @@
 //returns the index of the first encounter of a target element (if it is found) or -1 if that element does not exist within the input array.
 
 // INDEXOF //
-Array.prototype.myIndexOf = function() {
-
+Array.prototype.myIndexOf = function(includedElement, index) {
+    for(let i = (index || 0); i < this.length; i++){
+        if(this[i] === includedElement){
+            return i;
+        }
+    }
+    return -1;
 };
